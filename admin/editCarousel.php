5f547282d8f080a6af4e354b7770e14ba2197b29
dieu <dieu@192.168.4.3>
2014-12-19 12:03:37 +0100

    [EN COURS] - Carousel BO

--- admin/editCarousel.php
+++ admin/editCarousel.php
@@ -8,18 +8,33 @@ $img = htmlspecialchars($_FILES['inputImg']['name']);
 
 try {
     if(!empty($img)){
-    $db->exec("UPDATE `carousel` "
-            . "SET `source`='" . $source . "', `content`='" . $content . "' "
-            . "WHERE `id`='" . $id . "'");
+    $sql = "UPDATE carousel "
+         . "SET source=:source, content=:content "
+         . "WHERE id=:id";
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam(":source", $img, PDO::PARAM_STR, 255);
+    $stmt->bindParam(":content", $content, PDO::PARAM_STR, 100);
+    $stmt->bindParam(":id", $id, PDO::PARAM_INT, 12);
+    $stmt->execute();
+    /*$db->exec("UPDATE `carousel` "
+            . "SET `source`='" . $img . "', `content`='" . $content . "' "
+            . "WHERE `id`='" . $id . "'");*/
 
     $uploaddir = '../img/uploads_carousel/';
     $uploadfile = $uploaddir . basename($_FILES['inputImg']['name']);
 
     move_uploaded_file($_FILES['inputImg']['tmp_name'], $uploadfile);
     }else{
-    $db->exec("UPDATE `carousel` "
+    $sql = "UPDATE carousel "
+         . "SET content=:content "
+         . "WHERE id=:id";
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam(':content', $content, PDO::PARAM_STR, 100);
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT, 12);
+    $stmt->execute();
+    /*$db->exec("UPDATE `carousel` "
             . "SET `content`='" . $content . "' "
-            . "WHERE `id`='" . $id . "'");
+            . "WHERE `id`='" . $id . "'");*/
     }
     header('Location:homePageCarousel.php');
 
