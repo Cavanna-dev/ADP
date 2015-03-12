d38bc51ea1895eff054bce0e40ede39d2947f61e
LegerA <leger.au@gmail.com>
2015-03-12 17:16:08 +0100

    ok

--- admin/addCategory.php
+++ admin/addCategory.php
@@ -27,35 +27,62 @@ include 'listCategory.php';
         </div>
     <?php } ?>
 
-    <form class="form-horizontal" action="model/addCategory.php" method="POST">
-        <fieldset>
-            <legend>Ajouter une catégorie</legend>
-            <div class="form-group">
-                <label for="name" class="col-lg-2 control-label">Nom</label>
-                <div class="col-lg-2">
-                    <input type="text" class="form-control" name="name" id="name"
-                           value="<?php if (isset($_GET['name'])){echo $_GET['name'];}?>"/>
+    <div class="col-lg-6">
+        <form class="form-horizontal" action="model/addCategory.php" method="POST">
+            <fieldset>
+                <legend>Ajouter une catégorie</legend>
+                <div class="form-group">
+                    <label for="name" class="col-lg-2 control-label">Nom</label>
+                    <div class="col-lg-6">
+                        <input type="text" class="form-control" name="name" id="name"
+                               value="<?php if (isset($_GET['name'])){echo $_GET['name'];}?>"/>
+                    </div>
                 </div>
-            </div>
 
-            <div class="form-group">
-                <label for="idParent" class="col-lg-2 control-label">Catégorie parent</label>
-                <div class="col-lg-4">
-                    <select class="form-control" name="idParent" id="idParent">
-                        <option value="0"></option>
-                        <?php
+                <div class="form-group">
+                    <label for="idParent" class="col-lg-2 control-label">Catégorie parent</label>
+                    <div class="col-lg-10">
+                        <select class="form-control" name="idParent" id="idParent">
+                            <option value="0"></option>
+                            <?php
                             selectArray($list, $listId, '');
-                        ?>
-                    </select>
+                            ?>
+                        </select>
+                    </div>
                 </div>
-            </div>
-            <input type="hidden" name="idAdminUser" value="<?php echo $_SESSION['user_id'];?>"/>
-            <div class="form-group">
-                <div class="col-lg-10 col-lg-offset-2">
-                    <button type="submit" class="btn btn-primary">Sauvegarder</button>
+                <input type="hidden" name="idAdminUser" value="<?php echo $_SESSION['user_id'];?>"/>
+                <div class="form-group">
+                    <div class="col-lg-10 col-lg-offset-2">
+                        <button type="submit" class="btn btn-primary">Sauvegarder</button>
+                    </div>
                 </div>
-            </div>
-        </fieldset>
-    </form>
+            </fieldset>
+        </form>
+    </div>
+
+    <div class="col-lg-6">
+        <form class="form-horizontal" action="model/addCategory.php" method="POST">
+            <fieldset>
+                <legend>Gérer les catégories</legend>
+                <div class="form-group">
+                    <label for="idParent" class="col-lg-2 control-label">Catégorie parent</label>
+                    <div class="col-lg-10">
+                        <select class="form-control" name="idParent" id="idParent">
+                            <option value="0"></option>
+                            <?php
+                            selectArray($list, $listId, '');
+                            ?>
+                        </select>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-lg-10 col-lg-offset-2">
+                        <button type="submit" class="btn btn-primary">Sauvegarder</button>
+                    </div>
+                </div>
+            </fieldset>
+        </form>
+    </div>
+
 </div>
 <?php include 'template/footer.php'; ?>
