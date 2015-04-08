c6750fe1511e8471f9b65e4ef6bf8c465fb59fb0
LegerA <leger.au@gmail.com>
2015-04-08 18:51:26 +0200

    Correction

--- admin/modalCarousel.php
+++ admin/modalCarousel.php
@@ -2,41 +2,42 @@
 
 <div class="modal" id="modalAdd">
     <div class="modal-dialog">
-        <form class="form-horizontal" action="modelCarousel/addCarousel.php" method="POST" enctype="multipart/form-data">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                    <h4 class="modal-title">Ajouter une photo</h4>
-                </div>
-                <div class="modal-body">
-                    <fieldset>
-                        <div class="form-group">
-                            <label for="inputContent" class="col-lg-2 control-label">Contenu</label>
-                            <div class="col-lg-10">
-                                <input type="text" class="form-control" id="inputContent" name="inputContent" placeholder="Contenu Photo">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="inputImg" class="col-lg-2 control-label">Upload photo</label>
-                            <div class="col-lg-10">
-                                <input type="file" class="form-control" id="inputImg" name="inputImg">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="inputIsHp" class="col-lg-2 control-label">Page d'Accueil</label>
-                            <div class="col-lg-10">
-                                <div class="checkbox">
-                                    <label>
-                                        <input name="inputIsHp" type="checkbox" value="1">
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-                    </fieldset>
-                </div>
-                <div class="modal-footer"><button type="submit" class="btn btn-primary">Sauvegarder</button>
-                </div>
+        <div class="modal-content">
+        <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                <h4 class="modal-title">Ajouter une photo</h4>
             </div>
-        </form>
+            <div class="modal-body">
+                <form class="form-horizontal" action="modelCarousel/addCarousel.php" method="POST" enctype="multipart/form-data">
+
+                <fieldset>
+                    <div class="form-group">
+                        <label for="inputContent" class="col-lg-2 control-label">Contenu</label>
+                        <div class="col-lg-10">
+                            <input type="text" class="form-control" id="inputContent" name="inputContent" placeholder="Contenu Photo">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputImg" class="col-lg-2 control-label">Upload photo</label>
+                        <div class="col-lg-10">
+                            <input type="file" class="form-control" id="inputImg" name="inputImg">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputIsHp" class="col-lg-2 control-label">Page d'Accueil</label>
+                        <div class="col-lg-10">
+                            <div class="checkbox">
+                                <label>
+                                    <input name="inputIsHp" type="checkbox" value="1">
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </fieldset>
+            </div>
+            <div class="modal-footer"><button type="submit" class="btn btn-primary">Sauvegarder</button>
+            </div>
+            </form>-->
+        </div>        
     </div>
 </div>
