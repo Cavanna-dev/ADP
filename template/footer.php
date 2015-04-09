8c6094fe609a46930f95c4aedc7ba71ff1fc4ce8
LegerA <leger.au@gmail.com>
2015-04-09 12:41:42 +0200

    Footer

--- template/footer.php
+++ template/footer.php
@@ -20,12 +20,14 @@
                 <ul class="list-unstyled">
                     <li class="pull-right"><a href="#top">Back to top</a></li>
                     <li><a href="contact.php">Contact</a></li>
+                    <li><a href="form_login.php">Inscription</a></li>
                 </ul>
-                <p>Made by <a href="#" rel="nofollow">
-                        LEGER 'GodLike' Aurélien
-                    </a>. Contact his assistant at <a href="mailto:cavannachristophe@gmail.com">cavannachristophe@gmail.com
+                <p>Odysseus <?= date('Y') ?></p>
+<!--                <p>Made by <a href="#" rel="nofollow">
+                        TTLS
+                    </a>. Contact him at <a href="mailto:cavannachristophe@gmail.com">contact@ttls.com
                     </a>.</p>
-                <p>Code released under the <a href="https://github.com/thomaspark/bootswatch/blob/gh-pages/LICENSE">MIT License</a>.</p><p>Based on <a href="http://getbootstrap.com" rel="nofollow">Bootstrap</a>. Icons from <a href="http://fortawesome.github.io/Font-Awesome/" rel="nofollow">Font Awesome</a>. Web fonts from <a href="http://www.google.com/webfonts" rel="nofollow">Google</a>.</p>
+                <p>Code released under the <a href="https://github.com/thomaspark/bootswatch/blob/gh-pages/LICENSE">MIT License</a>.</p><p>Based on <a href="http://getbootstrap.com" rel="nofollow">Bootstrap</a>. Icons from <a href="http://fortawesome.github.io/Font-Awesome/" rel="nofollow">Font Awesome</a>. Web fonts from <a href="http://www.google.com/webfonts" rel="nofollow">Google</a>.</p>-->
             </div>
         </div>
     </div>
